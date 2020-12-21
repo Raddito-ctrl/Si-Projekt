c3f35b7a630733a990937d41036b695e5971a1c6
Raddito-ctrl <73673793+Raddito-ctrl@users.noreply.github.com>
2020-12-21 09:19:41 +0100

    Add files via upload

---
+++ Si/src/main/java/com/sample/Answer.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Answer{
+    public String question;
+    public String answer;
+
+    public Answer(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
